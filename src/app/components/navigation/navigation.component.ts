1bbd6c8bc7e999626377f680993ec98adfee7513
Dany Paredes <danywalls4@gmail.com>
2024-02-16 16:43:19 +0100

    update to angular 17.2.1 and navigation

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ROUTE_TOKENS } from '../../route-token';
 import { JsonPipe } from '@angular/common';
+import { NavigationModule } from '@progress/kendo-angular-navigation';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, JsonPipe],
+  imports: [RouterLink, RouterLinkActive, JsonPipe, NavigationModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
