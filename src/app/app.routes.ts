b6bfa46495d5e704c4175bc4de6c29f99d15075f
Dany Paredes <danywalls4@gmail.com>
2024-02-16 12:02:46 +0100

    feat: Add imports and routes to app component
    - Import NavigationComponent in app.component.ts
    - Update imports array in app.component.ts to include RouterOutlet and NavigationComponent
    - Import HomeComponent, NotFoundComponent, aboutRoutes, ROUTE_TOKENS, TeamComponent, CompanyComponent, and AboutComponent in app.routes.ts
    - Update routes array in app.routes.ts to include a default route, home route, about routes, and a wildcard route for not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { aboutRoutes } from './pages/about/about.routes';
+import { ROUTE_TOKENS } from './route-token';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: ROUTE_TOKENS.HOME,
+    pathMatch: 'full',
+  },
+  {
+    path: ROUTE_TOKENS.HOME,
+    component: HomeComponent,
+  },
+  aboutRoutes,
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
