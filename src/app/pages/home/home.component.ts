1186e912ad03370cd55d01c43dea521a42c1bc08
Dany Paredes <danywalls4@gmail.com>
2024-02-17 10:00:40 +0100

    feat: use router service navigation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ROUTE_TOKENS } from '../../route-token';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  readonly router = inject(Router);
+  public acceptTerms(value: boolean) {
+    if (value) {
+      this.router.navigate([ROUTE_TOKENS.ABOUT]);
+    }
+  }
 }
